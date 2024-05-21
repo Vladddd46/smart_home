fba7ab6a869a283523f78bc73499274ee9feb2af
vladddd46 <vladddd46@ukr.net>
2024-05-21 14:06:50 +0300

    improved

--- main.py
+++ main.py
@@ -1,19 +1,20 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, render_template
+import json
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def home():
-    return "Welcome to the Flask server!"
+    return render_template('index.html')
 
 @app.route('/control', methods=['POST'])
 def handle_data():
-    if request.is_json:
-        data = request.get_json()
-        print("Received data: ", data)
-        return jsonify({"result_code": "OK", "message": ""}), 200
-    else:
-        return jsonify({"result_code": "WRONG_FORMAT", "message": "request must be json"}), 400
+    json_input = request.form.get('jsonInput')
+    try:
+        data = json.loads(json_input)
+        return render_template('result.html', data=data)
+    except json.JSONDecodeError:
+        return "Invalid JSON", 400
 
 if __name__ == '__main__':
     app.run(debug=True)
